2a4304b76736f52a0e59954af37fce543da1d870
michellec20 <cc18064@ues.edu.sv>
2023-10-22 01:39:02 -0600

    Formularios Iniciales

---
+++ src/formularios/NodoUsuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formularios;
+
+public class NodoUsuario {
+    
+    //Atributos
+    String user, pass;
+    NodoUsuario siguiente;
+    
+    //Constructor
+    public NodoUsuario(String user, String pass){
+        this.user = user;
+        this.pass = pass;
+        siguiente = null;
+    }
+}
